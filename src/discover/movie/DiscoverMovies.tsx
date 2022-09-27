159c7e0f182428c5deab11b9760881462884cebd
Onur Alp Bicer <onuralpbicer@gmail.com>
2022-09-27 22:45:18 +1000

    some styling

--- src/discover/movie/DiscoverMovies.tsx
+++ src/discover/movie/DiscoverMovies.tsx
@@ -1,19 +1,30 @@
+import styled from 'styled-components'
 import Scroller from '../../shared/Scroller/Scroller'
 import { useDiscover } from '../util'
+
+const StyledHeading = styled.h1`
+	margin-block-end: 0;
+`
+
+const StyledSection = styled.section`
+	:not(:first-of-type) {
+		margin-top: 3rem;
+	}
+`
 
 const DiscoverMovies: React.FC = () => {
 	const listQuery = useDiscover('movie')
 
 	const list = listQuery.data ?? []
 	return (
-		<section>
-			<h1>Discover Movies</h1>
+		<StyledSection>
+			<StyledHeading>Discover Movies</StyledHeading>
 			{listQuery.isLoading ? (
 				'loading'
 			) : (
 				<Scroller list={list} imageKey="backdrop_path" titleKey="title" />
 			)}
-		</section>
+		</StyledSection>
 	)
 }
 
--- src/discover/tv/DiscoverTV.tsx
+++ src/discover/tv/DiscoverTV.tsx
@@ -1,19 +1,30 @@
+import styled from 'styled-components'
 import Scroller from '../../shared/Scroller/Scroller'
 import { useDiscover } from '../util'
+
+const StyledHeading = styled.h1`
+	margin-block-end: 0;
+`
+
+const StyledSection = styled.section`
+	:not(:first-of-type) {
+		margin-top: 3rem;
+	}
+`
 
 const DiscoverTVShows: React.FC = () => {
 	const listQuery = useDiscover('tv')
 
 	const list = listQuery.data ?? []
 	return (
-		<section>
-			<h1>Discover TV Shows</h1>
+		<StyledSection>
+			<StyledHeading>Discover TV Shows</StyledHeading>
 			{listQuery.isLoading ? (
 				'loading'
 			) : (
 				<Scroller list={list} imageKey="backdrop_path" titleKey="name" />
 			)}
-		</section>
+		</StyledSection>
 	)
 }
 
